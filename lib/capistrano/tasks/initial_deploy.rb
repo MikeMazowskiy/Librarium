4bb1eecda518bdec10c27b0bcc6684193269b042
MikeMazowskiy <vairelviper@gmail.com>
2023-07-31 09:06:13 +0300

    Fixed webpack

--- lib/capistrano/tasks/initial_deploy.rb
+++
@@ -1,9 +0,0 @@
-namespace :deploy do
-  desc "Initial server start"
-  task :initiall do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-end
