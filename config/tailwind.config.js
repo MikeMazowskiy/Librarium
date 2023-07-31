43a98e8e8342c200e09ae53719712e08ba06043f
MikeMazowskiy <vairelviper@gmail.com>
2023-07-31 16:06:20 +0300

    changed tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,28 +2,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   important: true,
-  purge: {
-    enabled: false,
-    content: [
-      './app/**/*.{html.erb,erb,js,css}',
-      './app/components/*.{html.erb,erb,js,css}',
-      './app/components/**/*.{html.erb,erb,js,css}',
-      './app/components/**/**/*.{html.erb,erb,js,css}',
-      './app/helpers/*.rb',
-    ],
-    options: {
-      safelist: [
-        'grid',
-        'grid-cols-4',
-        'grid-cols-7',
-      ],
-    },
-  },
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/**/*.{html.erb,erb,js,css}',
+    './app/components/*.{html.erb,erb,js,css}',
+    './app/components/**/*.{html.erb,erb,js,css}',
+    './app/components/**/**/*.{html.erb,erb,js,css}',
+    './app/helpers/*.rb',
   ],
   theme: {
     extend: {
